fea69e84855e9f23d2325205b570bbc103ccc336
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-17 19:01:25 -0300

    Web project added

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Clone do Twitter</title>
+      </Head>
+
+      <main>
+        <h1>Em breve...</h1>
+      </main>
+    </div>
+  )
+}
