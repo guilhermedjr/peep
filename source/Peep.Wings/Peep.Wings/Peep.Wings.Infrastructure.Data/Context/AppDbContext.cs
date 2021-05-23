96c9ac51732cc2a2568a91f499995dd0fa4949fa
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-23 11:01:44 -0300

    Added AppDbContext and User Mapping

---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Infrastructure.Data/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Peep.Wings.Domain.Entities;
+using Peep.Wings.Infrastructure.Data.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace Peep.Wings.Infrastructure.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+        }
+    }
+}
---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Infrastructure.Data/Mapping/UserMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Peep.Wings.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Peep.Wings.Infrastructure.Data.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Name);
+            builder.Property(u => u.Username);
+            builder.Property(u => u.Email);
+        }
+    }
+}
