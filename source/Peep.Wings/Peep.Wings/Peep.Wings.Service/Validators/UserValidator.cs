8babc55738cb9489cf537562da7377cf64a2c407
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-23 12:05:36 -0300

    Added BaseService and UserValidator

---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Service/Validators/UserValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using Peep.Wings.Domain.Entities;
+
+namespace Peep.Wings.Service.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Name)
+                .NotEmpty().WithMessage("Please enter the name")
+                .NotNull().WithMessage("Please enter the name");
+
+            RuleFor(u => u.Email)
+                .NotEmpty().WithMessage("Please enter the email")
+                .NotNull().WithMessage("Please enter the email");
+
+            RuleFor(u => u.Username)
+                .NotEmpty().WithMessage("Please enter the username")
+                .NotNull().WithMessage("Please enter the username");
+
+
+        }
+    }
+}
