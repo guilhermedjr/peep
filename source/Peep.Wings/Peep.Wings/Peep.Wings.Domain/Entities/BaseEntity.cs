5c54fa909e31de378bb7b637fd65626699cba4ac
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-23 11:00:04 -0300

    Added BaseEntity and User entities

---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peep.Wings.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; set; }
+    }
+}
---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peep.Wings.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
