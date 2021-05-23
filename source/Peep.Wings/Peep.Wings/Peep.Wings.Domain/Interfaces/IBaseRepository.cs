c567cd7b29b0a4c569c67f557bd90451d227d9fe
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-23 10:58:15 -0300

    Added BaseRepository and BaseService interfaces

---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Peep.Wings.Domain.Entities;
+
+namespace Peep.Wings.Domain.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        void Insert(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(int id);
+
+        Task<IList<TEntity>> Get();
+
+        Task<TEntity> Get(int id);
+    }
+}
---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Domain/Interfaces/IBaseService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using Peep.Wings.Domain.Entities;
+
+namespace Peep.Wings.Domain.Interfaces
+{
+    public interface IBaseService<TEntity> where TEntity : BaseEntity
+    {
+        TEntity Add<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+
+        void Delete(int id);
+
+        IList<TEntity> Get();
+
+        TEntity GetById(int id);
+
+        TEntity Update<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+    }
+}
