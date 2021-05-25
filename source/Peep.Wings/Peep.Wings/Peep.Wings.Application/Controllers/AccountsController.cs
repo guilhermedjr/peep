dde2549da49ad326a0836866d195f13dc07ec16a
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-24 23:30:06 -0300

    AccountsController added and appsettings updated

---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Application/Controllers/AccountsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using Peep.Wings.Domain.Entities;
+using Peep.Wings.Domain.Services;
+
+namespace Peep.Wings.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ITokenService _tokenService;
+        private readonly IEmailService _mailService;
+        private readonly ISmsService _smsService;
+
+        public AccountsController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ITokenService tokenService,
+            IEmailService mailService,
+            ISmsService smsService)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+            this._tokenService = tokenService;
+            this._mailService = mailService;
+            this._smsService = smsService;
+        }
+    }
+}
