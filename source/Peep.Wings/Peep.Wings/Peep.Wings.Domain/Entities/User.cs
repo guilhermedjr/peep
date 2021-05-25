a7e52ec4fec464d47f9e49cc499f851db0c18758
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-24 21:18:24 -0300

    Updated User entity

--- source/Peep.Wings/Peep.Wings/Peep.Wings.Domain/Entities/User.cs
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Domain/Entities/User.cs
@@ -9,5 +9,8 @@ namespace Peep.Wings.Domain.Entities
         public string Name { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BirthDate { get; set; }
+        public string Password { get; set; }
     }
 }
