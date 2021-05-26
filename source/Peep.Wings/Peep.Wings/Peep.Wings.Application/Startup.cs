1eb32fda6f0a1374cb4d2a21199676639d817d5e
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-26 20:21:22 -0300

    Identity settings; decoupled services configuration

--- source/Peep.Wings/Peep.Wings/Peep.Wings.Application/Startup.cs
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Application/Startup.cs
@@ -1,14 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.EntityFrameworkCore;
+using Peep.Wings.Domain.Entities;
 using Peep.Wings.Infrastructure.Data;
+using Peep.Wings.Infrastructure.IoC;
 
 namespace Peep.Wings.Application
 {
@@ -21,7 +20,6 @@ namespace Peep.Wings.Application
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -30,31 +28,14 @@ namespace Peep.Wings.Application
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Peep.Wings.Application", Version = "v1" });
             });
 
-            var key = Encoding.ASCII.GetBytes(Configuration["Jwt:Key"]);
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
+            services
+                .AddDefaultIdentity<ApplicationUser>(Bootstrapper.ConfigureIdentity)
+                .AddRoles<ApplicationRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
 
-            string mySqlConnection = Configuration.GetConnectionString("DefaultConnection");
-
-            services.AddDbContextPool<AppDbContext>(options =>
-                options.UseMySQL(mySqlConnection));
-
-           
+            Bootstrapper.ConfigureServices(services, Configuration);
+            Bootstrapper.ConfigureAuthentication(services, Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Infrastructure.CrossCutting/Bootstrapper.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
+using Peep.Wings.Domain.Services;
+using Peep.Wings.Infrastructure.Data;
+using Peep.Wings.Service.Services;
+
+namespace Peep.Wings.Infrastructure.IoC
+{
+    public static class Bootstrapper
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            string mySqlConnection = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContextPool<AppDbContext>(options =>
+                options.UseMySQL(mySqlConnection));
+
+            services.AddScoped<IEmailService, EmailService>();
+            services.AddScoped<ISmsService, SmsService>();
+            services.AddScoped<ITokenService, TokenService>();
+        }
+
+        public static void ConfigureIdentity(IdentityOptions options)
+        {
+            options.Password.RequiredLength = 6;
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredUniqueChars = 0;
+
+            options.User.RequireUniqueEmail = true;
+
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+            options.SignIn.RequireConfirmedAccount = false;
+        }
+
+        public static void ConfigureAuthentication(IServiceCollection services, IConfiguration configuration)
+        {
+            var key = Encoding.ASCII.GetBytes(configuration["Jwt:Key"]);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+    }
+}
