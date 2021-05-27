2ae49ade670d38541c38e2c56a4ec4a309a434d1
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-05-27 00:26:25 -0300

    Added User ViewModel

---
+++ source/Peep.Wings/Peep.Wings/Peep.Wings.Application/ViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Peep.Wings.Application.ViewModels
+{
+    public class ApplicationUserViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+    }
+}
