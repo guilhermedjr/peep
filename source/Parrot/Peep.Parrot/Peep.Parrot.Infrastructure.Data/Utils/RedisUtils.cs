1a82bcb19a5084af997ece8f75c5bb7e6c1974e3
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-06-07 17:48:50 -0300

    Added methods for conversion between objects and redis hashes

---
+++ source/Parrot/Peep.Parrot/Peep.Parrot.Infrastructure.Data/Utils/RedisUtils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using StackExchange.Redis;
+
+namespace Peep.Parrot.Infrastructure.Data.Utils
+{
+    public static class RedisUtils
+    {
+        public static HashEntry[] ObjectToHashEntries(object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            return properties
+                .Where(x => x.GetValue(obj) != null)
+                .Select
+                (
+                    property =>
+                    {
+                        object propertyValue = property.GetValue(obj);
+                        string hashValue;
+
+                        if (propertyValue is IEnumerable<object>)
+                        {
+                            hashValue = JsonSerializer.Serialize(propertyValue);
+                        }
+                        else
+                        {
+                            hashValue = propertyValue.ToString();
+                        }
+
+                        return new HashEntry(property.Name, hashValue);
+                    }
+                )
+                .ToArray();
+        }
+
+        public static T HashEntriesToObject<T>(HashEntry[] hashEntries)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            var obj = Activator.CreateInstance(typeof(T));
+
+            foreach (var property in properties)
+            {
+                HashEntry entry = hashEntries.FirstOrDefault(he => he.Name.ToString().Equals(property.Name));
+                if (entry.Equals(new HashEntry())) continue;
+                property.SetValue(obj, Convert.ChangeType(entry.Value.ToString(), property.PropertyType));
+            }
+            return (T)obj;
+        }
+    }
+}
