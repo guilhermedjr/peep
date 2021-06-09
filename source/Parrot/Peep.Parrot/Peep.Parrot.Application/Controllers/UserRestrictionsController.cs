c52abe1ae53a2eda3bc7bd859944e3b56a060693
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-06-09 12:38:19 -0300

    Routes to mute/unmute and block/unblock a user, and change account visibility

---
+++ source/Parrot/Peep.Parrot/Peep.Parrot.Application/Controllers/UserRestrictionsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Peep.Parrot.Domain.Dtos;
+using Peep.Parrot.Domain.Entities;
+using Peep.Parrot.Domain.Repository;
+
+namespace Peep.Parrot.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRestrictionsController : ControllerBase
+    {
+        private readonly IDistributedCache _cache;
+        private readonly IUserRestrictionsRepository _userRestrictionsRepository;
+
+        public UserRestrictionsController(
+            IDistributedCache cache,
+            IUserRestrictionsRepository userRestrictionsRepository)
+        {
+            this._cache = cache;
+            this._userRestrictionsRepository = userRestrictionsRepository;
+        }
+
+        [HttpPut]
+        public IActionResult ChangeAccountVisibility([FromQuery] Guid userId)
+        {
+            if (String.IsNullOrEmpty(userId.ToString()))
+                return BadRequest(new { Message = "User Id not specified" });
+
+             _userRestrictionsRepository.ChangeAccountVisibility(userId);
+             return NoContent();
+        }
+
+
+        [HttpPost]
+        [Route("MuteUser")]
+        public async Task<IActionResult> MuteUser([FromQuery] Guid mufflerId, [FromQuery] Guid mutedUserId)
+        {
+            if (String.IsNullOrEmpty(mufflerId.ToString()) ||
+               String.IsNullOrEmpty(mutedUserId.ToString()))
+            {
+                return BadRequest(new { Message = "Parameters are null" });
+            }
+
+            if (mufflerId == mutedUserId)
+                return BadRequest(new { Message = "The Muffler Id and the Muted User Id are the same" });
+
+            if (await _userRestrictionsRepository.MuteUser(mufflerId, mutedUserId))
+                return Ok();
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [Route("BlockUser")]
+        public async Task<IActionResult> BlockUser([FromQuery] Guid blockingUserId, [FromQuery] Guid blockedUserId)
+        {
+            if (String.IsNullOrEmpty(blockingUserId.ToString()) ||
+               String.IsNullOrEmpty(blockedUserId.ToString()))
+            {
+                return BadRequest(new { Message = "Parameters are null" });
+            }
+
+            if (blockingUserId == blockedUserId)
+                return BadRequest(new { Message = "The Blocking User Id and the Blocked User Id are the same" });
+
+            if (await _userRestrictionsRepository.BlockUser(blockingUserId, blockedUserId))
+                return Ok();
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("UnmuteUser")]
+        public async Task<IActionResult> UnmuteUser([FromQuery] Guid mufflerId, [FromQuery] Guid mutedUserId)
+        {
+            if (String.IsNullOrEmpty(mufflerId.ToString()) ||
+              String.IsNullOrEmpty(mutedUserId.ToString()))
+            {
+                return BadRequest(new { Message = "Parameters are null" });
+            }
+
+            if (mufflerId == mutedUserId)
+                return BadRequest(new { Message = "The Muffler Id and the Muted User Id are the same" });
+
+            if (await _userRestrictionsRepository.UnmuteUser(mufflerId, mutedUserId))
+                return NoContent();
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("UnblockUser")]
+        public async Task<IActionResult> UnblockUser([FromQuery] Guid blockingUserId, [FromQuery] Guid blockedUserId)
+        {
+            if (String.IsNullOrEmpty(blockingUserId.ToString()) ||
+               String.IsNullOrEmpty(blockedUserId.ToString()))
+            {
+                return BadRequest(new { Message = "Parameters are null" });
+            }
+
+            if (blockingUserId == blockedUserId)
+                return BadRequest(new { Message = "The Blocking User Id and the Blocked User Id are the same" });
+
+            if (await _userRestrictionsRepository.UnblockUser(blockingUserId, blockedUserId))
+                return NoContent();
+            return BadRequest();
+        }
+
+    }
+}
---
+++ source/Parrot/Peep.Parrot/Peep.Parrot.Repositories/UserRestrictionsRepository.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Peep.Parrot.Domain.Repository;
+using Peep.Parrot.Infrastructure.Data;
+
+namespace Peep.Parrot.Repositories
+{
+    public class UserRestrictionsRepository : RedisDbConnection, IUserRestrictionsRepository
+    {
+        private readonly 
+            IUsersConnectionsRepository _usersConnectionsRepository;
+
+        public UserRestrictionsRepository(
+            IConfiguration config,
+            IUsersConnectionsRepository usersConnectionsRepository
+            ): base(config) 
+        {
+            this._usersConnectionsRepository = usersConnectionsRepository;
+        }
+
+        public async Task<bool> MuteUser(Guid mufflerId, Guid mutedUserId)
+        {
+            if (!await base.GuidIsOnSet($"muted_user:{mufflerId}", mutedUserId))
+                return await base.AddGuidOnSet($"muted_user:{mufflerId}", mutedUserId);
+            return false;
+        }
+
+        public async Task<bool> BlockUser(Guid blockingUserId, Guid blockedUserId)
+        {
+            if (!await BlockageExistsAsync(blockingUserId, blockedUserId))
+            {
+                if (await BlockedUserFollowsBlockingUserAsync(blockedUserId, blockingUserId))
+                {
+                    if (await RemoveFollowAsync(blockedUserId, blockingUserId))
+                    {
+                        if (await BlockingUserFollowsBlockedUserAsync(blockingUserId, blockedUserId))
+                        {
+                            if (await RemoveFollowAsync(blockingUserId, blockedUserId))
+                                return await this.BlockUserAsync(blockingUserId, blockedUserId);
+                            return false;
+                        }
+                        return await this.BlockUserAsync(blockingUserId, blockedUserId);
+                    }
+                    return false;
+                }
+
+                else if (await BlockingUserFollowsBlockedUserAsync(blockingUserId, blockedUserId))
+                {
+                    if (await RemoveFollowAsync(blockingUserId, blockedUserId))
+                        return await this.BlockUserAsync(blockingUserId, blockedUserId);
+                    return false;
+                }
+
+                else 
+                    return await this.BlockUserAsync(blockingUserId, blockedUserId);
+            }
+            return false;
+        }
+
+        public async Task<bool> UnmuteUser(Guid mufflerId, Guid mutedUserId)
+        {
+            if (await base.GuidIsOnSet($"muted_user:{mufflerId}", mutedUserId))
+                return await base.DeleteGuidOfSet($"muted_user:{mufflerId}", mutedUserId);
+            return false;
+        }
+
+        public async Task<bool> UnblockUser(Guid blockingUserId, Guid blockedUserId)
+        {
+            if (await BlockageExistsAsync(blockingUserId, blockedUserId))
+                return await base.DeleteGuidOfSet($"blocked_user:{blockingUserId}", blockedUserId);
+            return false;
+        }
+
+        public async void ChangeAccountVisibility(Guid userId)
+        {
+            bool accountVisibility = await base.GetBooleanValueOfHashField($"user:{userId}", "IsPrivateAccount");
+            base.SetValueOfHashField<bool>($"user:{userId}", "IsPrivateAccount", !accountVisibility);
+        }
+
+        private async Task<bool> BlockageExistsAsync(Guid blockingUserId, Guid blockedUserId) =>
+            await base.GuidIsOnSet($"blocked_user:{blockingUserId}", blockedUserId);
+
+        private async Task<bool> BlockedUserFollowsBlockingUserAsync(Guid blockedUserId, Guid blockingUserId) =>
+            await _usersConnectionsRepository.ConnectionExists(blockedUserId, blockingUserId);
+
+        private async Task<bool> BlockingUserFollowsBlockedUserAsync(Guid blockingUserId, Guid blockedUserId) =>
+            await _usersConnectionsRepository.ConnectionExists(blockingUserId, blockedUserId);
+
+        private async Task<bool> RemoveFollowAsync(Guid followerId, Guid followedId) =>
+            await _usersConnectionsRepository.RemoveConnectionAsync(followerId, followedId);
+
+        private async Task<bool> BlockUserAsync(Guid blockingUserId, Guid blockedUserId) =>
+           await base.AddGuidOnSet($"blocked_user:{blockingUserId}", blockedUserId);
+    }
+}
