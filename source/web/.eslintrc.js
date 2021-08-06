14502a3ca406c2066328132971b99b74117106e6
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-08-06 13:28:16 -0300

    Added eslint and prettier

---
+++ source/web/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {extensions: ['.ts', '.tsx']}
+    ],
+    'import/prefer-default-export':
+  'off',
+  },
+};
