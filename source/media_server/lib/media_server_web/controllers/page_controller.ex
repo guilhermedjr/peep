be9256389dcd8480acb71d404c284881771cd7c1
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-08-17 12:20:37 -0300

    Remove media server and organize folders

--- source/media_server/lib/media_server_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule MediaServerWeb.PageController do
-  use MediaServerWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- source/media_server/lib/media_server_web/views/layout_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule MediaServerWeb.LayoutView do
-  use MediaServerWeb, :view
-end
