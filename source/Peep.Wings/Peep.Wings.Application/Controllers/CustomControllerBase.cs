f38deed1d70bb164e1250f868d32124305557bc8
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-08-29 15:27:10 -0300

    Add RabbitMQ to pub/sub

--- source/Peep.Wings/Peep.Wings.Application/Controllers/CustomControllerBase.cs
+++ source/Peep.Wings/Peep.Wings.Application/Controllers/CustomControllerBase.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
 using Peep.Wings.Domain.Entities;
 using Peep.Wings.Application.ViewModels;
 
@@ -13,10 +16,17 @@ namespace Peep.Wings.Application.Controllers
     public abstract class CustomControllerBase : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ConnectionFactory _factory;
+        private const string QUEUE_NAME = "wings_messages";
 
         public CustomControllerBase(UserManager<ApplicationUser> userManager)
         {
             this._userManager = userManager;
+
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
         }
 
         public abstract Task<IActionResult> ConfirmEmail([FromQuery] string token, [FromQuery] string email);
@@ -35,5 +45,28 @@ namespace Peep.Wings.Application.Controllers
         protected async Task<ApplicationUser> GetAuthenticatedUserAccount()
             => await _userManager.Users.FirstOrDefaultAsync(u => u.Id == LoggedUser.Id);
 
+        protected AcceptedResult PostCreatedAccountMessage(ApplicationUserViewModel user)
+        {
+            using var connection = _factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: QUEUE_NAME,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var stringfiedMessage = JsonSerializer.Serialize(user);
+            var bytesMessage = Encoding.UTF8.GetBytes(stringfiedMessage);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: QUEUE_NAME,
+                basicProperties: null,
+                body: bytesMessage);
+
+            return Accepted();
+        }
     }
 }
---
+++ source/Peep.Wings/Peep.Wings.Application/Controllers/MessagesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Peep.Wings.Domain.Dtos;
+using Peep.Wings.Domain.Entities;
+using Peep.Wings.Domain.Services;
+using Peep.Wings.Application.ViewModels;
+
+
+namespace Peep.Wings.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        // Test controller 
+        private readonly ConnectionFactory _factory;
+        private const string QUEUE_NAME = "wings_messages";
+
+        public MessagesController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+
+        [HttpPost]
+        public IActionResult PostCreatedAccountMessage([FromBody] ApplicationUserViewModel user)
+        {
+            using var connection = _factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: QUEUE_NAME,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var stringfiedMessage = JsonSerializer.Serialize(user);
+            var bytesMessage = Encoding.UTF8.GetBytes(stringfiedMessage);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: QUEUE_NAME,
+                basicProperties: null,
+                body: bytesMessage);
+
+            return Accepted();
+        }
+    }
+}
