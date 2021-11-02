d588152e2b55f561c519eae7ce721ee61bf6db86
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-11-02 18:15:09 -0300

    Add Entity abstraction

---
+++ source/Peep.Parrot/Peep.Parrot.Shared/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿namespace Peep.Parrot.Shared.Entities;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; private set; }
+}
+
