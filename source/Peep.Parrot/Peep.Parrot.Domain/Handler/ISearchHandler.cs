f4b38ada4a40fe8be8338420d1fb8bd7202091f1
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-12-14 16:44:24 -0300

    Add SearchHandler and SearchRepository interfaces

---
+++ source/Peep.Parrot/Peep.Parrot.Domain/Handler/ISearchHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace Peep.Parrot.Domain.Handler;
+
+public interface ISearchHandler
+{
+    Task<JsonDocument> Search(string searchString);
+    Task<IEnumerable<ApplicationUser>> SearchUsers(string searchString);
+}
+
---
+++ source/Peep.Parrot/Peep.Parrot.Domain/Repository/ISearchRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Peep.Parrot.Domain.Repository;
+
+public interface ISearchRepository<T> where T : class
+{
+    Task<IEnumerable<T>> Search(string searchString);
+}
+
