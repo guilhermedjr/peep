ae951dde22abd2fbec5cf0a510ae5d257de6ab08
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2022-03-01 12:54:34 -0300

    Update User.cs

--- source/timeline-service/Peep.Timeline/Entities/User.cs
+++ source/timeline-service/Peep.Timeline/Entities/User.cs
@@ -2,20 +2,17 @@
 
 public class User
 {
+    private readonly IList<User> _following;
+    private readonly IList<User> _blockedUsers;
+    private readonly IList<User> _mutedUsers;
+
     public Guid Id { get; private set; }
     public string Email { get; private set; }
     public string Name { get; private set; }
     public string Username { get; private set; }
-    public string ProfileImageUrl { get; private set; }
-    public DateOnly BirthDate { get; private set; }
-    public DateTime JoinedAt { get; private set; }
-
-    public string Bio { get; private set; }
-    public string Location { get; private set; }
-    public string Website { get; private set; }
-
-    public bool PrivateAccount { get; private set; }
-    public bool VerifiedAccount { get; private set; }
-
     public IEnumerable<Peep> Peeps { get; set; }
+    public IEnumerable<User> Following { get; set; }
+    public IEnumerable<User> BlockedUsers { get; set; }
+    public IEnumerable<User> MutedUsers { get; set; }
+
 }
